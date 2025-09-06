a10a764a188d1b42af58b3d6d54f6071c48f9b39
parthopaul <parthopaul.dev@gmail.com>
2025-09-06 00:54:48 -0700

    news categories article load and show feature added

---
+++ script/script.js
@@ -0,0 +1,82 @@
+let getElement = (id) => document.getElementById(id);
+
+// nav categories load 
+let loadData = async (showContainer) => {
+  let data = await fetch('https://news-api-fs.vercel.app/api/categories');
+  let useableDatas = await data.json();
+  allData=useableDatas.categories
+  showData(allData,showContainer)
+  
+
+};
+// news by categories load 
+let loadNewsByCategories = async (id) => {
+  let data = await fetch(`https://news-api-fs.vercel.app/api/categories/${id}`);
+  let newsDatas = await data.json();
+  let allNews=newsDatas.articles;
+  showNewsByCategories(allNews)
+
+  
+  
+  
+
+};
+// nav item show feature
+let showData = (useableDatas, showContainer) => {
+  showContainer.innerHTML = " ";
+
+  useableDatas.forEach((item) => {
+    
+    showContainer.innerHTML += `
+        <a id='${item.id}' class='hover:border-b-2 pb-[10px] hover:border-b[red]' href="#">${item.title}</a>
+
+        `;
+  });
+  let firstNavItem=getElement('main');
+firstNavItem.classList.add('active')
+};
+
+let showNewsByCategories=(allNews)=>{
+    let newsContainer=getElement('news-container') ;
+    newsContainer.innerHTML=' ';
+    allNews.forEach(news => {
+      
+        
+        newsContainer.innerHTML +=`
+        <div class="w-[300px]  p-[20px] pb-[20px] bg-[#efecec]">
+    <img class='w-full h-[150px] mb-[10px]' src="${news.image.srcset[1].url}" alt="">
+    <h2 class=' font-bold mb-[15px] '>${news.title}</h2>
+    <h2 class='  '>${news.time}</h2>
+
+</div>
+        `
+    });
+}
+//get nav containeter 
+let navContainer = getElement("nav-links-box");
+// called nav item loaddata function
+   loadData(navContainer);
+
+
+
+
+// nav item active status add and remove feature
+navContainer.addEventListener("click", (e) => {
+  let allNavItem = navContainer.querySelectorAll("a");
+
+  // remove active class
+  allNavItem.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  // add class
+  if (e.target.localName === "a") {
+
+    loadNewsByCategories(e.target.id)
+    e.target.classList.add("active");
+  }
+});
+// first show main categories articles
+loadNewsByCategories('main')
+
+
